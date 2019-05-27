95da029699145e0e5319427fdb110eb8b59686cb
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2019-05-27 17:00:03 -0600

    Add files via upload

---
+++ AIP Label Creation.ps1
@@ -0,0 +1,11 @@
+﻿Connect-AadrmService
+
+
+$names = @{}
+$names[1033] = "Encrypt"
+$descriptions = @{}
+$descriptions[1033] = "Encrypt Docs and Emails"
+
+$r1 = New-AadrmRightsDefinition -Domain tminus365.com -Rights "Owner"
+
+Add-AadrmTemplate -Names $names -Descriptions $descriptions -RightsDefinitions $r1 -ScopedIdentities "7c0947591@tminus365.com" -Status Published
