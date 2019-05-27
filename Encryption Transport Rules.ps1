65543abfa0d17363401161b4c201c8612009d7bc
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2019-05-27 17:02:25 -0600

    Add files via upload

---
+++ Encryption Transport Rules.ps1
@@ -0,0 +1,12 @@
+﻿$credential = Get-Credential
+
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+
+New-TransportRule -Name "Encrypt Email" -SubjectContainsWords "Encrypt" -ApplyRightsProtectionTemplate "Encrypt"
+
+Set-IRMConfiguration -DecryptAttachmentForEncryptOnly $true
+New-TransportRule -Name "Encrypt outbound sensitive emails (out of box rule)" -SentToScope  NotInOrganization  -ApplyRightsProtectionTemplate "Encrypt" -MessageContainsDataClassifications @(@{Name="ABA Routing Number"; minCount="1"},@{Name="Credit Card Number"; minCount="1"},@{Name="Drug Enforcement Agency (DEA) Number"; minCount="1"},@{Name="U.S. / U.K. Passport Number"; minCount="1"},@{Name="U.S. Bank Account Number"; minCount="1"},@{Name="U.S. Individual Taxpayer Identification Number (ITIN)"; minCount="1"},@{Name="U.S. Social Security Number (SSN)"; minCount="1"}) -Notifysender "NotifyOnly"
