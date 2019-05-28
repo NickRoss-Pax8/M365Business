584d9bfd9083a9831b2b87749619f3ed3a8a6886
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2019-05-27 21:26:04 -0600

    Add files via upload

---
+++ Bulk Upload 365 Distribution LIst and Members.ps1
@@ -0,0 +1,26 @@
+﻿##########Connect to Exchange Online##########
+
+Write-Host -Prompt "Connecting to Exchange Online"
+
+$credential = Get-Credential
+
+Install-module Msonline
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+
+
+############# Define CSV path of Users and Group ##################
+
+
+$DistributionGroups = Read-Host -Prompt "Enter File Path For CSV list of Distro Groups"
+
+$DistributionGroupMembers = Read-Host -Prompt "Enter File Path For CSV list of Distro Group Members"
+
+
+############Import CSVs###############################
+
+Import-Csv -Path $DistributionGroups| foreach {New-Distributiongroup -Name $_.Name -PrimarySmtpAddress $_.PrimarySmtpAddress }
+
+Import-Csv $DistributionGroupMembers | foreach {Add-DistributionGroupMember -Identity $_.DL -Member $_.Alias}
