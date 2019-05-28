d3fd4c59d1bb33db4c26e33e8b7488892f8c8eb2
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2019-05-27 21:20:18 -0600

    Add files via upload

---
+++ Bulk Updating Users License.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module MSOnline
+$cred = Get-Credential
+Connect-MsolService -Credential $cred
+ 
+$oldLicense = "czptsws:ENTERPRISEPACK"
+$newLicense = "czptsws:ENTERPRISEPREMIUM"
+
+Import-Csv -Path 'FilePath'
+ 
+$users = Get-MsolUser -MaxResults 5000 | Where-Object { $_.isLicensed -eq "TRUE" }
+
+Import-Csv -Path 'FilePath'
+ 
+foreach ($user in $users){
+    $upn = $user.UserPrincipalName
+    foreach ($license in $user.Licenses) {
+        if ($license.AccountSkuId -eq $oldLicense) {
+            $disabledPlans = @()
+            Write-Host("User $upn will go from $oldLicense to $newLicense and will have no options disabled.")
+            Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $newLicense -RemoveLicenses $oldLicense
+        }
+    }
