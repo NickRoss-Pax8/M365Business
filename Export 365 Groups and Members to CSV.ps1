284f8b6f3dc94f4e3d70fd477db8f1c28cc11fb2
NickRoss-Pax8 <49160790+NickRoss-Pax8@users.noreply.github.com>
2019-05-27 21:23:19 -0600

    Add files via upload

---
+++ Export 365 Groups and Members to CSV.ps1
@@ -0,0 +1,35 @@
+﻿##The cmdlet written below will export all the groups as well as members to a CSV file##
+
+##########Connect to Exchange Online##########
+
+Write-Host -Prompt "Connecting to Exchange Online"
+
+$credential = Get-Credential
+
+Install-module Msonline
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+
+
+###Export Groups and Members##############################
+
+$Result=@()
+$groups = Get-DistributionGroup -ResultSize Unlimited
+$totalmbx = $groups.Count
+$i = 1
+$groups | ForEach-Object {
+Write-Progress -activity "Processing $_.DisplayName" -status "$i out of $totalmbx completed"
+$group = $_
+Get-DistributionGroupMember -Identity $group.Name -ResultSize Unlimited | ForEach-Object {
+$member = $_
+$Result += New-Object PSObject -property @{
+GroupName = $group.DisplayName
+Member = $member.Name
+EmailAddress = $member.PrimarySMTPAddress
+RecipientType= $member.RecipientType
+}}
+$i++
+}
+$Result | Export-CSV "C:\users\nickross\documents\All-Distribution-Group-Members.csv" -NoTypeInformation -Encoding UTF8
